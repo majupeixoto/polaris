bf3e413f7b83a244cf2199afe2f04bdc55b25690
Maju <mjpo@cesar.school>
2024-09-27 12:17:41 -0300

    add mais algumas coisasa

--- Projeto/apps/models.py
+++ Projeto/apps/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# lembrando: SEMPRE que modificar ou acrescentar uma models a gnt tem q fazer os comandos:
+# python manage.py makemigrations
+# e em seguida:
+# python manage.py migrate
+# python manage.py runserver
--- Projeto/pol/settings.py
+++ Projeto/pol/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +120,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
+STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+
+# Diretórios de arquivos static
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
