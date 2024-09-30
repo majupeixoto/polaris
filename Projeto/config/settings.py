6775dcc22477b223076016eedd2745a7d3713e7a
Maju <mjpo@cesar.school>
2024-09-30 18:57:27 -0300

    push login deu certo

--- Projeto/config/settings.py
+++ Projeto/config/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+         'DIRS': [
+            BASE_DIR / 'base'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
