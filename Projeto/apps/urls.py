ec3e55871fc434142f4dc94e3526f4c0f9a88ba8
Maju <mjpo@cesar.school>
2024-09-30 18:54:15 -0300

    commit logins

--- Projeto/apps/urls.py
+++ Projeto/apps/urls.py
@@ -3,5 +3,5 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    
+    path('', views.login_view, name='login'), 
 ]
--- Projeto/apps/views.py
+++ Projeto/apps/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import Perfil
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        trocar_perfil = request.POST.get('trocar_perfil')
+
+        if trocar_perfil == 'aluno':
+            return redirect(...)
+
+        elif trocar_perfil == 'funcionario':
+            return redirect(...)
+
+    return render(request, 'apps/login.html')
+
+def logout_view(request):
+    logout(request)
+    if "usuario" in request.session:
+        del request.session["usuario"]
+    return redirect('login')
