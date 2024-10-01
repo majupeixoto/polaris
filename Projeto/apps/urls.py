330cc8a26a41174dad1cffe74694ef6fb000c5a9
TH <155742254+aguiarth@users.noreply.github.com>
2024-09-30 21:43:13 -0300

    criando login + css

--- Projeto/apps/urls.py
+++ Projeto/apps/urls.py
@@ -3,5 +3,6 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'), 
+    path('', views.login_view, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), 
 ]
